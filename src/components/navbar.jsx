23df6bdf8a9b1ad4dd5eb5b692741aa2f8470eea
Lagadnakul <nakullagad084@gmail.com>
2025-02-12 16:54:16 +0530

    push

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,45 +1,29 @@
 import React from "react";
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
-const navbar = () => {
-  const linkClass=({isActive}) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2':'"text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+const Navbar = () => {
+  const linkClass = ({isActive}) => 
+    isActive 
+      ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+      : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            {/* <!-- Logo --> */}
-            <NavLink
-              className="flex flex-shrink-0 items-center mr-4"
-             to="/"
-            >
-              <img
-                className="h-10 w-auto"
-                src="logo.png"
-                alt="React Jobs"
-              />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                React Jobs
-              </span>
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <NavLink className="text-white text-2xl font-bold" to="/">
+              React Jobs
             </NavLink>
-            <div className="md:ml-auto">
-              <div className="flex space-x-2">
-                <NavLink
-                  to="/"
-                  className={linkClass}
-                >
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                <NavLink className={linkClass} to="/">
                   Home
                 </NavLink>
-                <NavLink
-                  to="/jobs"
-                  className={linkClass}
-                >
+                <NavLink className={linkClass} to="/jobs">
                   Jobs
                 </NavLink>
-                <NavLink
-                 to="/add"
-                 className={linkClass}
-                >
+                <NavLink className={linkClass} to="/add">
                   Add Job
                 </NavLink>
               </div>
@@ -51,4 +35,4 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export default Navbar;
